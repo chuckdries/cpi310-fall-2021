1d30b652fb32b517f281c447de14fffc9352fd97
Chuck Dries <chuck@chuckdries.com>
2021-11-08 16:32:34 -0800

    format index.js

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-import express from 'express';
-import exphbs from 'express-handlebars';
+import express from "express";
+import exphbs from "express-handlebars";
 
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 const dbPromise = open({
-  filename: './databases/database.db',
-  driver: sqlite3.Database
+  filename: "./databases/database.db",
+  driver: sqlite3.Database,
 });
 
 const app = express();
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 
-app.get('/', async function (req, res) {
+app.get("/", async function (req, res) {
   const db = await dbPromise;
-  const messages = await db.all('SELECT * FROM Message;');
-  res.render('home', { messages });
-})
+  const messages = await db.all("SELECT * FROM Message;");
+  res.render("home", { messages });
+});
 
-app.post('/message', async (req, res) => {
-  console.log('body', req.body);
+app.post("/message", async (req, res) => {
+  console.log("body", req.body);
   const db = await dbPromise;
-  await db.run('INSERT INTO Message (text) VALUES (?)', req.body.message);
-  res.redirect('/');
-})
+  await db.run("INSERT INTO Message (text) VALUES (?)", req.body.message);
+  res.redirect("/");
+});
 
 const setup = async () => {
   const db = await dbPromise;
   db.migrate({ force: false });
-  app.listen(8080, () => { console.log('listening on http://localhost:8080') });
-}
+  app.listen(8080, () => {
+    console.log("listening on http://localhost:8080");
+  });
+};
 
 setup();
