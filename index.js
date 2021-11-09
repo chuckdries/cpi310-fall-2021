c1a57155143eb6d4b491a053ac68ca915028ffe8
Chuck Dries <chuck@chuckdries.com>
2021-11-08 16:29:38 -0800

    store messages in database

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
+import express from 'express';
+import exphbs from 'express-handlebars';
+
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+const dbPromise = open({
+  filename: './databases/database.db',
+  driver: sqlite3.Database
+});
 
 const app = express();
 
@@ -8,16 +16,23 @@ app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded())
 
-const messages = [];
-
-app.get('/', function (req, res) {
+app.get('/', async function (req, res) {
+  const db = await dbPromise;
+  const messages = await db.all('SELECT * FROM Message;');
   res.render('home', { messages });
 })
 
-app.post('/message', (req, res) => {
+app.post('/message', async (req, res) => {
   console.log('body', req.body);
-  messages.push(req.body.message);
+  const db = await dbPromise;
+  await db.run('INSERT INTO Message (text) VALUES (?)', req.body.message);
   res.redirect('/');
 })
 
-app.listen(8080)
+const setup = async () => {
+  const db = await dbPromise;
+  db.migrate({ force: false });
+  app.listen(8080, () => { console.log('listening on http://localhost:8080') });
+}
+
+setup();
